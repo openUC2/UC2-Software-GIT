6e42d2fbb25ba902df871bd45dafe475c9a93d04
Alec <vercruysse.alec@gmail.com>
2020-07-01 12:41:21 -0700

    change WORKING_DIR variable in scripts
    - this pointed to a hard-coded link that was now broken.
    - this line from s/o: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself worked for my limited testing.-

--- GUI/RASPBERRY_PI/SCRIPTS/SETUP_UC2env/00-UC2_Prerequisites.sh
+++ GUI/RASPBERRY_PI/SCRIPTS/SETUP_UC2env/00-UC2_Prerequisites.sh
@@ -2,6 +2,6 @@
 
 echo [UC2] Loading variables...
 USER_HOME_DIR="/home/$USER"
-WORKING_DIR="${USER_HOME_DIR}/UC2/SCRIPTS"
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ARCHITECTURE=$(uname -m)
 UC2_ENVIRONMENT_NAME="UC2env"
