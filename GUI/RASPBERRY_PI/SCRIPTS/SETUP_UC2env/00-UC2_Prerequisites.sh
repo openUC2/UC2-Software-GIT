6011a18323594626840a1486b012f7c690e96c05
René Lachmann <herr.rene.richter@gmail.com>
2020-07-03 14:34:57 +0200

    Update GUI/RASPBERRY_PI/SCRIPTS/SETUP_UC2env/00-UC2_Prerequisites.sh
    install into active instead of prenamed directory
    
    Co-authored-by: Alec <vercruysse.alec@gmail.com>

--- GUI/RASPBERRY_PI/SCRIPTS/SETUP_UC2env/00-UC2_Prerequisites.sh
+++ GUI/RASPBERRY_PI/SCRIPTS/SETUP_UC2env/00-UC2_Prerequisites.sh
@@ -2,6 +2,6 @@
 
 echo [UC2] Loading variables...
 USER_HOME_DIR="/home/$USER"
-WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WORKING_DIR="$(pwd)"
 ARCHITECTURE=$(uname -m)
 UC2_ENVIRONMENT_NAME="UC2env"
